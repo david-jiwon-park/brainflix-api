7769dfdad0886e9af5e976cf01ad31d7f72da486
david-jiwon-park <davidpark48@hotmail.com>
2024-05-17 23:23:33 -0400

    Making minor updates

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 require('dotenv').config();
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT;
 const videoRoutes = require("./routes/videos");
 const cors = require("cors");
 
